2ae101badc89dcd9911f07df1a1cf9c5748cf439
tonypincheng <160933846+tonypincheng@users.noreply.github.com>
2025-04-07 18:14:53 +0000

    Deploying to gh-pages from @ tonypincheng/tonypincheng.github.io@7e98f72b10b938a93ac0b4f84a119df1a86b2968 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -58,13 +58,6 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/books/";
               },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
             },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
